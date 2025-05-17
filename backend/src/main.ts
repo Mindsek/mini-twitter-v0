99b7bbf078365ab0af7e9619fcfcb20cd66254d0
Antonin <contactmindsek@gmail.com>
2025-05-17 15:36:06 +0200

    feat: add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,31 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', process.env.FRONTEND_URL],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Mini twitter')
+    .setDescription('The Mini twitter API description')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3001);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+void bootstrap();
